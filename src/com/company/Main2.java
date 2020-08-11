3dad851852b5dd405f1cdc20b8106342b760c1ed
Aaronli <52963434+zhishui0501@users.noreply.github.com>
2020-08-11 20:57:03 +1000

    3 commit

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
 	// write your code
@@ -8,6 +8,5 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("finish");
-        System.out.println("fan");
     }
 }
