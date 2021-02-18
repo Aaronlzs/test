77e7ef468a54c6189307ad09804985c882a526ea
Aaronli <52963434+zhishui0501@users.noreply.github.com>
2021-02-18 13:28:16 +1100

    test commit

---
+++ src/com/company/Main3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        // write your code here  branchmerge
+        for (Rank r: Rank.values()) {
+            System.out.println(r);
+        }
+
+    }
+}
